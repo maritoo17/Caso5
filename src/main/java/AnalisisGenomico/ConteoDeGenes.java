73fb0255c58d718c0db9ee25d1fcd649c65e18f8
maritoo17 <msancgim@myuax.com>
2024-04-11 18:23:49 +0200

    2

--- src/main/java/AnalisisGenomico/ConteoDeGenes.java
+++ src/main/java/AnalisisGenomico/ConteoDeGenes.java
@@ -1,4 +1,42 @@
 package AnalisisGenomico;
 
 public class ConteoDeGenes {
+
+    public static void main(String[] args) {
+        String dna = "ATGAAATGAAAAATGTAGTAAATGAGAGAGATGTAATAG";
+        int count = contarGenes(dna);
+        System.out.println("Total de genes encontrados: " + count);
+    }
+
+    public static int contarGenes(String dna) {
+        return contarGenesAux(dna, 0);
+    }
+
+    private static int contarGenesAux(String dna, int index) {
+        if (dna.length() - index < 3) {
+            return 0;
+        }
+
+        int startIndex = dna.indexOf("ATG", index);
+        if (startIndex == -1) {
+            return 0;
+        }
+
+        int endIndex = encontrarFinalGen(dna, startIndex + 3);
+        if (endIndex != -1) {
+            return 1 + contarGenesAux(dna, endIndex + 3);
+        } else {
+            return contarGenesAux(dna, startIndex + 3);
+        }
+    }
+
+    private static int encontrarFinalGen(String dna, int startIndex) {
+        for (int i = startIndex; i < dna.length() - 2; i += 3) {
+            String codon = dna.substring(i, i + 3);
+            if (codon.equals("TAG") || codon.equals("TAA") || codon.equals("TGA")) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
