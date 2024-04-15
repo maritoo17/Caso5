86274811e4bc0507d76a207e5c97591ce9222c4c
maritoo17 <msancgim@myuax.com>
2024-04-15 18:21:33 +0200

    13

---
+++ src/main/java/AnalisisNumerico/CalculadoraDeNumeros.java
@@ -0,0 +1,6 @@
+package AnalisisNumerico;
+
+public interface CalculadoraDeNumeros {
+    int calcularPotencia(int base, int exponente);
+    int encontrarMaximo(java.util.List<Integer> numeros, int indice, int maximoActual);
+}
--- src/main/java/AnalisisNumerico/PotenciasYMaximos.java
+++ src/main/java/AnalisisNumerico/PotenciasYMaximos.java
@@ -3,20 +3,10 @@ package AnalisisNumerico;
 import java.util.Arrays;
 import java.util.List;
 
-public class PotenciasYMaximos {
+public class PotenciasYMaximos implements CalculadoraDeNumeros {
 
-    public static void main(String[] args) {
-        int base = 2;
-        int exponente = 5;
-        int resultadoPotencia = calcularPotencia(base, exponente);
-        System.out.println(base + "^" + exponente + " = " + resultadoPotencia);
-
-        List<Integer> numeros = Arrays.asList(1, 3, 7, 2, 5);
-        int maximo = encontrarMaximo(numeros, 0, numeros.get(0));
-        System.out.println("El valor máximo en la lista es: " + maximo);
-    }
-
-    public static int calcularPotencia(int base, int exponente) {
+    @Override
+    public int calcularPotencia(int base, int exponente) {
         if (exponente == 0) {
             return 1;
         } else {
@@ -24,7 +14,8 @@ public class PotenciasYMaximos {
         }
     }
 
-    public static int encontrarMaximo(List<Integer> numeros, int indice, int maximoActual) {
+    @Override
+    public int encontrarMaximo(List<Integer> numeros, int indice, int maximoActual) {
         if (indice == numeros.size()) {
             return maximoActual;
         } else {
@@ -33,5 +24,17 @@ public class PotenciasYMaximos {
             }
             return encontrarMaximo(numeros, indice + 1, maximoActual);
         }
+    }
+
+    public static void main(String[] args) {
+        CalculadoraDeNumeros calculator = new PotenciasYMaximos();
+        int base = 2;
+        int exponente = 5;
+        int resultadoPotencia = calculator.calcularPotencia(base, exponente);
+        System.out.println(base + "^" + exponente + " = " + resultadoPotencia);
+
+        List<Integer> numeros = Arrays.asList(1, 3, 7, 2, 5);
+        int maximo = calculator.encontrarMaximo(numeros, 0, numeros.get(0));
+        System.out.println("El valor máximo en la lista es: " + maximo);
     }
 }
