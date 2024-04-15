e0e6c3a5261ce1fce86a06ce9df563cb30a4c1fb
maritoo17 <msancgim@myuax.com>
2024-04-15 18:23:40 +0200

    14

---
+++ src/main/java/AnalisisNumerico/OperacionesNumericas.java
@@ -0,0 +1,8 @@
+package AnalisisNumerico;
+
+import java.util.List;
+
+public interface OperacionesNumericas {
+    int sumaNaturales(int n);
+    List<Integer> listarNumeros(int inicio, int fin);
+}
--- src/main/java/AnalisisNumerico/SumatoriaYListado.java
+++ src/main/java/AnalisisNumerico/SumatoriaYListado.java
@@ -3,20 +3,10 @@ package AnalisisNumerico;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SumatoriaYListado {
+public class SumatoriaYListado implements OperacionesNumericas {
 
-    public static void main(String[] args) {
-        int n = 10;
-        int resultadoSuma = sumaNaturales(n);
-        System.out.println("La suma de los primeros " + n + " números naturales es: " + resultadoSuma);
-
-        int inicio = 1;
-        int fin = 10;
-        List<Integer> listaNumeros = listarNumeros(inicio, fin);
-        System.out.println("Listado de números del " + inicio + " al " + fin + ": " + listaNumeros);
-    }
-
-    public static int sumaNaturales(int n) {
+    @Override
+    public int sumaNaturales(int n) {
         if (n <= 0) {
             return 0;
         } else {
@@ -24,7 +14,8 @@ public class SumatoriaYListado {
         }
     }
 
-    public static List<Integer> listarNumeros(int inicio, int fin) {
+    @Override
+    public List<Integer> listarNumeros(int inicio, int fin) {
         if (inicio > fin) {
             return new ArrayList<>();
         } else {
@@ -32,5 +23,17 @@ public class SumatoriaYListado {
             lista.add(fin);
             return lista;
         }
+    }
+
+    public static void main(String[] args) {
+        OperacionesNumericas operations = new SumatoriaYListado();
+        int n = 10;
+        int resultadoSuma = operations.sumaNaturales(n);
+        System.out.println("La suma de los primeros " + n + " números naturales es: " + resultadoSuma);
+
+        int inicio = 1;
+        int fin = 10;
+        List<Integer> listaNumeros = operations.listarNumeros(inicio, fin);
+        System.out.println("Listado de números del " + inicio + " al " + fin + ": " + listaNumeros);
     }
 }
