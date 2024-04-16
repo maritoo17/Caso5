7d9a21f6be3e1ec9fae44a8efe406bfe1168216d
maritoo17 <msancgim@myuax.com>
2024-04-16 19:17:37 +0200

    17

--- src/main/java/InformacionGenetica/OrganizacionDeDocumentos.java
+++ src/main/java/InformacionGenetica/OrganizacionDeDocumentos.java
@@ -5,22 +5,24 @@ import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 
-public class OrganizacionDeDocumentos {
+public class OrganizacionDeDocumentos implements OrganizadorDeDocumentos {
+
+    @Override
+    public void organizeDocuments(String inputFile, String outputFile) {
+        try {
+            List<String> lineas = Files.readAllLines(Paths.get(inputFile));
+            Collections.sort(lineas);
+            Files.write(Paths.get(outputFile), lineas);
+            System.out.println("Las notas han sido organizadas y guardadas en " + outputFile);
+        } catch (Exception e) {
+            System.err.println("Ocurrió un error al intentar organizar las notas: " + e.getMessage());
+        }
+    }
 
     public static void main(String[] args) {
         String archivoEntrada = "src/main/java/InformacionGenetica/notas.txt";
         String archivoSalida = "src/main/java/InformacionGenetica/notas_ordenadas.txt";
-
-        try {
-            List<String> lineas = Files.readAllLines(Paths.get(archivoEntrada));
-
-            Collections.sort(lineas);
-
-            Files.write(Paths.get(archivoSalida), lineas);
-
-            System.out.println("Las notas han sido organizadas y guardadas en " + archivoSalida);
-        } catch (Exception e) {
-            System.err.println("Ocurrió un error al intentar organizar las notas: " + e.getMessage());
-        }
+        OrganizadorDeDocumentos organizer = new OrganizacionDeDocumentos();
+        organizer.organizeDocuments(archivoEntrada, archivoSalida);
     }
 }
---
+++ src/main/java/InformacionGenetica/OrganizadorDeDocumentos.java
@@ -0,0 +1,7 @@
+package InformacionGenetica;
+
+import java.util.List;
+
+public interface OrganizadorDeDocumentos {
+    void organizeDocuments(String inputFile, String outputFile);
+}
